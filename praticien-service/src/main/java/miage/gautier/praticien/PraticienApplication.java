050d4346ac77838cd6166b0b5d0e3a111677a3fa
Gautier BENOIT <gautier.benoit@etu.univ-cotedazur.fr>
2025-02-19 13:42:39 +0100

    initializing the project

---
+++ praticien-service/src/main/java/miage/gautier/praticien/PraticienApplication.java
@@ -0,0 +1,13 @@
+package miage.gautier.praticien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticienApplication.class, args);
+	}
+
+}
---
+++ praticien-service/src/test/java/miage/gautier/praticien/PraticienApplicationTests.java
@@ -0,0 +1,13 @@
+package miage.gautier.praticien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
